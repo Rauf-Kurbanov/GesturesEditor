458b39a90fb40a794777cdd2674e83073014c4d8
Abramov Ivan <airvan21@gmail.com>
2012-12-05 18:41:24 +0400

    scaling rects for rect && ellipse

--- editor0.2/GesturesEditor/ellipse.cpp
+++ editor0.2/GesturesEditor/ellipse.cpp
@@ -26,7 +26,43 @@ void Ellipse::drawItem(QPainter *painter, const QStyleOptionGraphicsItem *option
 }
 
 void Ellipse::drawScalingRects(QPainter *painter) {
-	painter->setPen(QPen(Qt::lightGray, 1, Qt::DashLine, Qt::RoundCap));
+	painter->setPen(QPen(Qt::darkGray, 1, Qt::DashLine, Qt::RoundCap));
 	painter->drawRect(this->boundingRect());
+	painter->setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
+	painter->setBrush(this->mBrush);
+	QRectF leftBottomScalRect;
+	QRectF leftTopScalRect;
+	QRectF rightTopScalRect;
+	QRectF rightBottomScalRect;
+	if (mX2 >= mX1) {
+		if (mY2 >= mY1) {
+			leftTopScalRect = QRectF(QPoint(mX1, mY1), QPoint(mX1 + scalingRect, mY1 + scalingRect));
+			leftBottomScalRect = QRectF(QPoint(mX1, mY2 - scalingRect), QPoint(mX1 + scalingRect, mY2));
+			rightBottomScalRect = QRectF(QPoint(mX2 - scalingRect, mY2 - scalingRect), QPoint(mX2, mY2));
+			rightTopScalRect = QRectF(QPoint(mX2 - scalingRect, mY1), QPoint(mX2, mY1 + scalingRect));
+
+		} else {
+			leftBottomScalRect = QRectF(QPoint(mX1, mY1 - scalingRect), QPoint(mX1 + scalingRect, mY1));
+			leftTopScalRect = QRectF(QPoint(mX1, mY2), QPoint(mX1 + scalingRect, mY2 + scalingRect));
+			rightTopScalRect = QRectF(QPoint(mX2 - scalingRect, mY2), QPoint(mX2 , mY2 + scalingRect));
+			rightBottomScalRect = QRectF(QPoint(mX2 - scalingRect, mY1 - scalingRect), QPoint(mX2, mY1));
+		}
+	} else {
+		if (mY2 <= mY1) {
+			leftBottomScalRect = QRectF(QPoint(mX2, mY1 - scalingRect), QPoint(mX2 + scalingRect, mY1));
+			leftTopScalRect = QRectF(QPoint(mX2, mY2), QPoint(mX2 + scalingRect, mY2 + scalingRect));
+			rightTopScalRect = QRectF(QPoint(mX1 - scalingRect, mY2), QPoint(mX1 , mY2 + scalingRect));
+			rightBottomScalRect = QRectF(QPoint(mX1 - scalingRect, mY1 - scalingRect), QPoint(mX1, mY1));
+		} else {
+			leftBottomScalRect = QRectF(QPoint(mX2, mY2 - scalingRect), QPoint(mX2 + scalingRect, mY2));
+			leftTopScalRect = QRectF(QPoint(mX2, mY1), QPoint(mX2 + scalingRect, mY1 + scalingRect));
+			rightTopScalRect = QRectF(QPoint(mX1 - scalingRect, mY1), QPoint(mX1 , mY1 + scalingRect));
+			rightBottomScalRect = QRectF(QPoint(mX1 - scalingRect, mY2 - scalingRect), QPoint(mX1, mY2));
+		}
+	}
+	painter->drawRect(leftTopScalRect);
+	painter->drawRect(rightBottomScalRect);
+	painter->drawRect(leftBottomScalRect);
+	painter->drawRect(rightTopScalRect);
 }
 
